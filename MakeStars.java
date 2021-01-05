11c055bee9dcf7417be8ad516f49966d926f6dd6
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-05 05:52:15 -0500

    just saving my progress, nothing special in this commit

--- MakeStars.java
+++ MakeStars.java
@@ -5,11 +5,20 @@ public class MakeStars{
       //use the standard input (terminal input)
       //as the string that you read from
       Scanner n = new Scanner( System.in );
+      while (n.hasNextLine()){
+        Scanner s = new Scanner (n.nextLine())
+        String temp = "";
+        temp = s.next();
+        for (int i=0; i< temp.length();i++){
+          System.out.print("*");
+        }
+        System.out.print(" ");
+      }
       //use hasNextLine()/nextLine() to loop over
       //all of the data
 
       //If you want to read the input word by word
-      //this can be replaced with hasNext() and next() 
+      //this can be replaced with hasNext() and next()
 
   }
 
