5d8e2cf44536657363e3417baff3192c6f07a0c4
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-05 06:01:49 -0500

    fixed the loop so it covered all words and added the line breaks in the correct location

--- MakeStars.java
+++ MakeStars.java
@@ -1,4 +1,4 @@
-import java.util*;
+import java.util.*;
 public class MakeStars{
 
   public static void main( String[]args ){
@@ -6,13 +6,16 @@ public class MakeStars{
       //as the string that you read from
       Scanner n = new Scanner( System.in );
       while (n.hasNextLine()){
-        Scanner s = new Scanner (n.nextLine())
+        Scanner s = new Scanner (n.nextLine());
         String temp = "";
-        temp = s.next();
-        for (int i=0; i< temp.length();i++){
-          System.out.print("*");
+        while (s.hasNext()){
+          temp = s.next();
+          for (int i=0; i< temp.length();i++){
+            System.out.print("*");
+          }
+          System.out.print(" ");
         }
-        System.out.print(" ");
+        System.out.println();
       }
       //use hasNextLine()/nextLine() to loop over
       //all of the data
